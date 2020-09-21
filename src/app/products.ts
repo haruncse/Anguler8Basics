b77a7a50ed962ef0430b7c1acaaf5d8478f3aace
haruncse <harunce12008@gmail.com>
2020-09-21 16:51:43 +0600

    Product list displayed

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        name:'Product 1',
+        price:200,
+        description:'Product Description 1'
+    },
+    {
+        name:'Product 2',
+        price:300,
+        description:'Product Description 2'
+    },
+    {
+        name:'Product 3',
+        price:400,
+        description:'Product Description 3'
+    },
+    {
+        name:'Product 4',
+        price:600,
+        description:'Product Description 4'
+    },
+];
